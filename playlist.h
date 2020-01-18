597a1f176e7056624f324aef42621fef7c506a8e
tumrusia <57960070+tumrusia@users.noreply.github.com>
2020-01-18 12:43:03 +0100

    wyjątki, trochę innych poprawek

--- playlist.h
+++ playlist.h
@@ -21,16 +21,14 @@ public:
         return this->file_description;
     }
 
-    File(const std::string& description) :
-            file_description(description)
+    File(std::string description) :
+            file_description(std::move(description))
     {}
 };
 
 
 class Piece : public Playable { };
 
-// Nie potrzebujemy konstruktorów kopiujących, bo będą zwracane tylko
-// wskaźniki na te obiekty; nie ma co się martwić polami.
 class Song : public Piece {
     const std::string artist;
     const std::string title;
@@ -53,9 +51,6 @@ class Movie : public Piece {
     const std::string title;
     const std::string year;
     const std::string contents;
-    /*
-     * TODO ROT13 dla contents w Movie
-     */
 
 public:
     void play() const override {
@@ -134,9 +129,6 @@ public:
             element->play();
     }
 
-/*
- * TODO nazwa playlisty się jakoś źle zapisuje
- */
     Playlist(const std::string& name) :
             tracklist(std::make_shared<collection_t>()),
             mode(createSequenceMode()),
@@ -197,12 +189,12 @@ private:
             if (line[i] >= 'a' && line[i] <= 'z') {
                 help = line[i] - 'a';
                 help = (help + 13) % 26 + 'a';
-                answer[i] = (char)help; //TODO wrong type of cast
+                answer[i] = static_cast<char>(help);
             }
             else if (line[i] >= 'A' && line[i] <= 'Z') {
                 help = line[i] - 'A';
                 help = (help + 13) % 26 + 'A';
-                answer[i] = (char)help; //TODO wrong type of cast
+                answer[i] = static_cast<char>(help);
             }
             else {
                 answer[i] = line[i];
@@ -213,6 +205,95 @@ private:
         return answer;
     }
 
+    void censorship(const std::string& line) {
+
+        char legalSpecial[8] = {',', '.', '!', '?', '\'', ':', ';', '-'};
+        bool ok;
+
+        for (char c : line) {
+
+            if (isalnum(c) || isspace(c)) {
+                continue;
+            }
+            else {
+                ok = false;
+                for (char s : legalSpecial) {
+                    if (c == s)
+                        ok = true;
+                }
+                if (!ok)
+                    throw CorruptContentException();
+            }
+        }
+    }
+
+    std::shared_ptr<Piece> openSong(const std::vector <std::string>& data) {
+
+        std::string artist, title, contents;
+        bool found_artist = false, found_title = false;
+
+        for (size_t i = 1; i < data.size(); i++) {
+            if (begEqual(data[i], "artist:")) {
+                artist = data[i].substr(7, data[i].size() - 7);
+                found_artist = true;
+            }
+            else if (begEqual(data[i], "title:")) {
+                title = data[i].substr(6, data[i].size() - 6);
+                found_title = true;
+            }
+            else if (i == data.size() - 1) {
+                censorship(data[i]);
+                contents = data[i];
+            }
+            else {
+                if (!findColon(data[i])) {
+                    throw CorruptContentException();
+                }
+            }
+        }
+
+        if (found_artist && found_title) {
+            return std::make_shared<Song>(artist, title, contents);
+        }
+        else {
+            throw CorruptContentException();
+        }
+    }
+
+    std::shared_ptr<Piece> openVideo(const std::vector <std::string>& data) {
+
+        std::string title, year, contents;
+        bool found_title = false, found_year = false;
+
+        for (size_t i = 1; i < data.size(); i++) {
+            if (begEqual(data[i], "title:")) {
+                title = data[i].substr(6, data[i].size() - 6);
+                found_title = true;
+            }
+            else if (begEqual(data[i], "year:")) {
+                year = data[i].substr(5, data[i].size() - 5);
+                found_year = true;
+            }
+            else if (i == data.size() - 1) {
+                censorship(data[i]);
+                contents = std::move(decipher(data[i]));
+
+            }
+            else {
+                if (!findColon(data[i])) {
+                    throw CorruptContentException();
+                }
+            }
+        }
+
+        if (found_title && found_year) {
+            return std::make_shared<Movie>(title, year, contents);
+        }
+        else {
+            throw CorruptContentException();
+        }
+    }
+
 public:
 
     std::shared_ptr<Piece> openFile(const File& file) {
@@ -221,53 +302,19 @@ public:
         std::vector <std::string> data;
         boost::algorithm::split(data, str, boost::is_any_of("|"));
 
-        //assert(data.size() >= 4);
-        bool found_artist = false, found_title = false, found_year = false;
-        std::string artist, title, year, contents;
+        if(data.size() < 4) {
+            throw CorruptFileException();
+        }
+
         std::string type = data[0];
-
-        for (size_t i = 1; i < data.size(); i++) {
-            if (begEqual(data[i], "artist:")) {
-                artist = data[i].substr(7, data[i].size() - 7);
-                if (found_artist) {
-                    //TODO exception
-                }
-                found_artist = true;
-            }
-            else if (begEqual(data[i], "title:")) {
-                title = data[i].substr(6, data[i].size() - 6);
-                if (found_title) {
-                    //TODO exception
-                }
-                found_title = true;
-            }
-            else if (begEqual(data[i], "year:")) {
-                year = data[i].substr(5, data[i].size() - 5);
-                if (found_year) {
-                    //TODO exception
-                }
-                found_year = true;
-            }
-            else if (i == data.size() - 1) {
-                contents = data[i];
-            }
-            else {
-                if (!findColon(data[i])) {
-                    //TODO exception
-                }
-            }
+        if (type == "audio") {
+            return openSong(data);
         }
-
-        if (type == "audio" && found_artist && found_title) {
-            return std::make_shared<Song>(artist, title, contents);
-        }
-        else if (type == "video" && found_year && found_title) {
-            contents = std::move(decipher(contents));
-            return std::make_shared<Movie>(title, year, contents);
+        else if (type == "video") {
+            return openVideo(data);
         }
         else {
-            //TODO exception
-            return nullptr;
+            throw UnsupportedTypeException();
         }
     };
 
